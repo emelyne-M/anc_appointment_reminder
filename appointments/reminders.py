666f926b7d59cc6742efba413b287537aef0e389
Mwiteneza emelyne <mwitenezaemelyne184@gmail.com>
2026-03-09 14:23:51 +0200

    Addition of ANC project

---
+++ appointments/reminders.py
@@ -0,0 +1,31 @@
+from datetime import date, timedelta
+from twilio.rest import Client
+from .models import Appointment, Reminder
+import os
+
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+twilio_number = os.getenv("TWILIO_PHONE_NUMBER")
+
+client = Client(account_sid, auth_token)
+
+def send_sms_reminders():
+    tomorrow = date.today() + timedelta(days=1)
+    
+    # Find pending appointments for tomorrow
+    appointments = Appointment.objects.filter(scheduled_date=tomorrow, status='pending')
+    
+    for appointment in appointments:
+        patient = appointment.patient
+        message = f"Hello {patient.full_name}, this is a reminder for your Antenatal care  Visit {appointment.visit_number} scheduled tomorrow."
+        
+        # Send SMS
+        client.messages.create(
+            body=message,
+            from_=twilio_number,
+            to=patient.phone
+        )
+        
+        # Save reminder log
+        Reminder.objects.create(appointment=appointment, reminder_type='SMS')
+        print(f"Reminder sent to {patient.full_name}")
